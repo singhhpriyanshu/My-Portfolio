dfea1c755ea1767a1b580ee1e978a7a172e6f8b9
Priyanshu singh <124794071+singhhpriyanshu@users.noreply.github.com>
2024-05-08 12:28:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
